1d94c99301fc923e0c0532426266c49b6cbd5af8
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-02-03 17:43:59 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -131,7 +131,7 @@ class MainMenuState extends MusicBeatState
 		}
 
 		#if MODS_ALLOWED
-		mods = new FlxSprite(820, -15).loadGraphic(Paths.image('mainmenu/MenuItems/mods'));
+		mods = new FlxSprite(820, -15).loadGraphic(Paths.image('mainmenu/menuItems/mods'));
 		mods.scale.x = 0.8;
 		mods.scale.y = 0.8;
 		add(mods);
