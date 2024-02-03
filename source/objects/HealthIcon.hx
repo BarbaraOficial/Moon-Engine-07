36b36a7297c58f21baa3b1d4b4eb2c4e989ffcd9
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-02-03 17:52:40 -0300

    Update HealthIcon.hx

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -32,12 +32,26 @@ class HealthIcon extends FlxSprite
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
 			
 			var graphic = Paths.image(name, allowGPU);
+			if (graphic.width == 450) {
+			loadGraphic(graphic, true, Math.floor(graphic.width / 3), Math.floor(graphic.height));
+			iconOffsets[0] = (width - 150) / 3;
+			iconOffsets[1] = (height - 150) / 3;
+			}
+			else
+			{
 			loadGraphic(graphic, true, Math.floor(graphic.width / 2), Math.floor(graphic.height));
 			iconOffsets[0] = (width - 150) / 2;
 			iconOffsets[1] = (height - 150) / 2;
+			}
 			updateHitbox();
 
-			animation.add(char, [0, 1], 0, false, isPlayer);
+			if (graphic.width == 450) {
+				animation.add(char, [0, 1, 2], 0, false, isPlayer);
+			}
+			else
+			{
+				animation.add(char, [0, 1], 0, false, isPlayer);
+			}
 			animation.play(char);
 			this.char = char;
 
