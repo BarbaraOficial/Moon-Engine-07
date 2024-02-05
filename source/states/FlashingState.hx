780e79e14b3376d986a28cd7e898cc712417100d
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-02-05 19:27:29 -0300

    Update FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -3,6 +3,8 @@ package states;
 import flixel.FlxSubState;
 
 import flixel.effects.FlxFlicker;
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
 import lime.app.Application;
 import flixel.addons.transition.FlxTransitionableState;
 
@@ -10,6 +12,7 @@ class FlashingState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
 
+	var background:FlxSprite;
 	var warnText:FlxText;
 	override function create()
 	{
@@ -17,8 +20,6 @@ class FlashingState extends MusicBeatState
 		
 		super.create();
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		add(bg);
 
 		var guh:String = "Hey, watch out!\n
 		This Mod contains some flashing lights!\n
@@ -26,11 +27,27 @@ class FlashingState extends MusicBeatState
 		Press B/ESCAPE to ignore this message.\n
 		You've been warned!";
 
+		background = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		background.scrollFactor.set();
+		background.updateHitbox();
+		background.screenCenter();
+		background.color = 0xFF353535;
+		add(background);
+
 		controls.isInSubstate = false; // qhar I hate it
 		warnText = new FlxText(0, 0, FlxG.width, guh, 32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
+
+		var creditsMusic:FlxText = new FlxText(11, FlxG.height - 40, 0, "Credits to Mario Party for the music mini games start", 25);
+		creditsMusic.setFormat("VCR OSD Mono", 25, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		creditsMusic.borderSize = 2.8;
+		add(creditsMusic);
+
+		var grid:FlxBackdrop = new FlxBackdrop(FlxGridOverlay.createGrid(80, 80, 160, 160, true, 0x70000000, 0x0));
+		grid.velocity.set(40, 40);
+		add(grid);
 
 		addVirtualPad(NONE, A_B);
 	}
@@ -46,6 +63,7 @@ class FlashingState extends MusicBeatState
 				if(!back) {
 					ClientPrefs.data.flashing = false;
 					ClientPrefs.saveSettings();
+					FlxTween.tween(background, {alpha: 0}, 0.25, {startDelay: 0.25});
 					FlxG.sound.play(Paths.sound('confirmMenu'));
 					if (FlxG.sound.music != null)
 				  	FlxTween.tween(FlxG.sound.music, {pitch: 0, volume: 0}, 1.5, {ease: FlxEase.sineInOut});
@@ -56,6 +74,7 @@ class FlashingState extends MusicBeatState
 					});
 				} else {
 					FlxG.sound.play(Paths.sound('cancelMenu'));
+					FlxTween.tween(background, {alpha: 0}, 0.25, {startDelay: 0.25});
 					if (FlxG.sound.music != null)
 					FlxTween.tween(FlxG.sound.music, {pitch: 0, volume: 0}, 2.5, {ease: FlxEase.sineInOut});
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
