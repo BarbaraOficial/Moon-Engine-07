d46009ac9b0d212cbf712967ea40220a9c4f1684
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2024-02-12 10:38:42 -0300

    Add files via upload

--- source/import.hx
+++ source/import.hx
@@ -21,6 +21,16 @@ import mobile.flixel.input.FlxMobileInputID;
 import mobile.backend.Data;
 import mobile.backend.SUtil;
 
+//Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+//import android.Tools as AndroidTools;
+#end
+
 #if sys
 import sys.*;
 import sys.io.*;
